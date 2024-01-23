32ff0d15611a305d2192ca04344c41126d8c5530
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-01-24 00:41:30 +0700

    Add Controller, Reoutes, and View

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gumilang - Movies</title>
+    {{-- style --}}
+    @vite('resoureces/css/app.css')
+</head>
+
+<body>
+    <div class="w-full h-auto min-h-screen flex flex-col">
+        {{-- header --}}
+        <div class="w-full bg-white h-[96px] drop-shadow-lg flex flex-row items-center">
+            <div class="w-1/3">
+                A
+            </div>
+
+            <div class="w-1/3">
+                B
+            </div>
+
+            <div class="w-1/3">
+                C
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
