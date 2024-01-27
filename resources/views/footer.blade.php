a838d29424e8add32d376800b5816ee20cc9fc69
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-01-27 18:06:08 +0700

    Add footer.blade.php component

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,25 @@
+<div class="w-full bg-sky-900 h-[320px] text-white flex flex-row pt-12">
+    {{-- Column Left --}}
+    <div class="w-6/12 pl-28 flex flex-col">
+        <span class="font-quicksand text-4xl font-bold">G - CINEMATION</span>
+        <span class="font-inter text-lg mt-4">Kami menyediakan review film dan TV series<br>yang bisa jadi
+            acuan
+            tontonanmu<br>#MoviesYouMustWatchBeforeYouDie.</span>
+        <span class="font-inter text-lg mt-5">&copy; 2024 Gumilang Dev</span>
+    </div>
+
+    {{-- Column Middle --}}
+    <div class="w-3/12 flex flex-col">
+        <span class="font-inter font-bold text-lg">Website</span>
+        <a href="/" class="font-inter text-lg mt-4 hover:text-sky-500 duration-200">Home</a>
+        <a href="/Movies" class="font-inter text-lg mt-4 hover:text-sky-500 duration-200">Movies</a>
+        <a href="/tv-shows" class="font-inter text-lg mt-4 hover:text-sky-500 duration-200">TV Shows</a>
+    </div>
+
+    {{-- Column Right --}}
+    <div class="w-3/12 flex flex-col">
+        <span class="font-inter font-bold text-lg">Social Media</span>
+        <a href="https://www.instagram.com/mgilangggggg" target="_blank"
+            class="font-inter text-lg hover:text-sky-500 duration-200">Instagram</a>
+    </div>
+</div>
