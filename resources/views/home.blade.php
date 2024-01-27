7afeada71d5d24f7597cdc06b219940a7dcbf789
mgilangggggg <muhamadgilangabdulgani@gmail.com>
2024-01-27 15:17:37 +0700

    Add show top tv shows

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,6 +61,7 @@
                     </svg>
                 </button>
             </div>
+            {{-- End Prev Button --}}
 
             {{-- Next Button --}}
             <div class="absolute right-0 top-1/2 -translate-y-1/2 w-1/12 flex justify-center" onclick="moveSlide(1)">
@@ -73,6 +74,7 @@
                     </svg>
                 </button>
             </div>
+            {{-- End Next Button --}}
 
             {{-- Indikator --}}
             <div class="absolute bottom-0 w-full mb-8">
@@ -82,13 +84,17 @@
                             onclick="currentSlide({{ $pos }})"></div>
                     @endfor
                 </div>
+                {{-- End Indikator --}}
             </div>
+            {{-- End Banner Section --}}
         </div>
 
         {{-- Top 10 Movies Section --}}
         <div class="mt-12">
             <span class="ml-28 font-inter font-bold text-xl">Top 10 Movies</span>
             <div class="w-auto flex flex-row overflow-auto pl-28 pt-6 pb-10">
+
+                {{-- Top 10 Movie Data --}}
                 @foreach ($topMovies as $movieItem)
                     @php
                         $original_date = $movieItem->release_date;
@@ -127,54 +133,86 @@
                 @endforeach
             </div>
         </div>
+        {{-- End Top 10 Movies Section --}}
 
-    </div>
-    <script>
-        // Deafult active slide
-        let slideIndex = 1;
-        showSlide(slideIndex);
+        {{-- Top 10 TV Shows Section --}}
+        <div>
+            <span class="ml-28 font-inter font-bold text-xl">Top 10 TV Shows</span>
+            <div class="w-auto flex flex-row overflow-auto pl-28 pt-6 pb-10">
 
-        function showSlide(position) {
-            let index;
-            const slidesArray = document.getElementsByClassName("slide");
-            const dotsArray = document.getElementsByClassName("dot");
+                <a href="movie/id" class="group">
+                    <div
+                        class="min-w-[232px] min-h-[428px] bg-white drop-shadow-[0_0px_8px_rgba(0,0,0,0.25)] group-hover:drop-shadow-[0_0px_8px_rgba(0,0,0,0.5)] rounded-[32px] p-5 flex flex-col mr-8 duration-100">
+                        <div class="overflow-hidden rounded-[32px]">
+                            <img src="https://via.placeholder.com/232x300" alt=""
+                                class="w-full h-[310px] rounded-[32px] group-hover:scale-125 duration-200">
+                        </div>
 
-            // Looping effect
-            if (position > slidesArray.length) {
-                slideIndex = 1;
+                        <span
+                            class="font-inter font-bold text-xl mt-4 line-clamp-1 group-hover:line-clamp-none">Title</span>
+                        <span class="font-inter text-sm mt-1">2024</span>
+
+                        <div class="flex flex-row mt-1 items-center">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="https://www.w3.org/2000/svg">
+                                <path
+                                    d="M18 21H8V8L15 1L16.25 2.25C16.3667 2.36667 16.4627 2.525 16.538 2.725C16.6127 2.925 16.65 3.11667 16.65 3.3V3.65L15.55 8H21C21.5333 8 22 8.2 22.4 8.6C22.8 9 23 9.46667 23 10V12C23 12.1167 22.9873 12.2417 22.962 12.375C22.9373 12.5083 22.9 12.6333 22.85 12.75L19.85 19.8C19.7 20.1333 19.45 20.4167 19.1 20.65C18.75 20.8833 18.3833 21 18 21ZM6 8V21H2V8H6Z"
+                                    fill="#008000"></path>
+                            </svg>
+                            <span class="font-inter text-sm ml-1">8.444</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        {{-- End Top 10 Tv Shows Section --}}
+
+        <script>
+            // Deafult active slide
+            let slideIndex = 1;
+            showSlide(slideIndex);
+
+            function showSlide(position) {
+                let index;
+                const slidesArray = document.getElementsByClassName("slide");
+                const dotsArray = document.getElementsByClassName("dot");
+
+                // Looping effect
+                if (position > slidesArray.length) {
+                    slideIndex = 1;
+                }
+
+                if (position < 1) {
+                    slideIndex = slidesArray.length;
+                }
+
+                // Hide all slides
+                for (index = 0; index < slidesArray.length; index++) {
+                    slidesArray[index].classList.add('hidden');
+                }
+
+                // Show active slides
+                slidesArray[slideIndex - 1].classList.remove('hidden');
+
+                // Remove active status
+                for (index = 0; index < dotsArray.length; index++) {
+                    dotsArray[index].classList.remove('bg-sky-500');
+                    dotsArray[index].classList.add('bg-white');
+                }
+
+                // Set active status
+                dotsArray[slideIndex - 1].classList.remove('bg-white');
+                dotsArray[slideIndex - 1].classList.add('bg-sky-500');
             }
 
-            if (position < 1) {
-                slideIndex = slidesArray.length;
+            function moveSlide(moveStep) {
+                showSlide(slideIndex += moveStep)
             }
 
-            // Hide all slides
-            for (index = 0; index < slidesArray.length; index++) {
-                slidesArray[index].classList.add('hidden');
+            function currentSlide(position) {
+                showSlide(slideIndex = moveStep)
             }
-
-            // Show active slides
-            slidesArray[slideIndex - 1].classList.remove('hidden');
-
-            // Remove active status
-            for (index = 0; index < dotsArray.length; index++) {
-                dotsArray[index].classList.remove('bg-sky-500');
-                dotsArray[index].classList.add('bg-white');
-            }
-
-            // Set active status
-            dotsArray[slideIndex - 1].classList.remove('bg-white');
-            dotsArray[slideIndex - 1].classList.add('bg-sky-500');
-        }
-
-        function moveSlide(moveStep) {
-            showSlide(slideIndex += moveStep)
-        }
-
-        function currentSlide(position) {
-            showSlide(slideIndex = moveStep)
-        }
-    </script>
+        </script>
 </body>
 
 </html>
